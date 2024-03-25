e2f0708cdebf48db03779f5c3ac0376bddec5343
vvcvv-as <142417101@qq.com>
2024-03-25 20:07:25 +0800

    add comments

--- PB21071458_万宸希_lab1/src/caches.py
+++ PB21071458_万宸希_lab1/src/caches.py
@@ -19,23 +19,27 @@ class L1Cache(Cache):
         self.mem_side = bus.cpu_side_ports
     
 class L1ICache(L1Cache):
+    # Set the default size
     size = '16kB'
-    
+    #add constructors for options
     def __init__(self, options=None):
         super(L1ICache, self).__init__(options)
         if not options or not options.l1i_size:
             return
         self.size = options.l1i_size
+    #port to connect to CPU
     def connectCPU(self, cpu):
         self.cpu_side = cpu.icache_port
 
 class L1DCache(L1Cache):
+    # Set the default size
     size = '64kB'
     def __init__(self, options=None):
         super(L1DCache, self).__init__(options)
         if not options or not options.l1d_size:
             return
         self.size = options.l1d_size
+    #port to connect to CPU
     def connectCPU(self, cpu):
         self.cpu_side = cpu.dcache_port
 	
@@ -47,11 +51,13 @@ class L2Cache(Cache):
     response_latency = 20
     mshrs = 20
     tgts_per_mshr = 12
+    #add constructors for options
     def __init__(self, options=None):
         super(L2Cache, self).__init__()
         if not options or not options.l2_size:
             return
         self.size = options.l2_size
+    #port to connect to CPU and MEM
     def connectCPUSideBus(self, bus):
     	self.cpu_side = bus.mem_side_ports
     def connectMemSideBus(self, bus):
--- PB21071458_万宸希_lab1/src/simple.py
+++ PB21071458_万宸希_lab1/src/simple.py
@@ -1,24 +1,31 @@
 import m5
 from m5.objects import *
-
+# create the system we are going to simulate
 system = System()
+
+# Set the clock frequency of the system (and all of its children)
 system.clk_domain = SrcClockDomain()
 system.clk_domain.clock = '1GHz'
 system.clk_domain.voltage_domain = VoltageDomain()
+# Set up the system
 system.mem_mode = 'timing'
 system.mem_ranges = [AddrRange('512MB')]
+# Create a simple CPU
 system.cpu = TimingSimpleCPU()
+# Create a memory bus, a system crossbar, in this case
 system.membus = SystemXBar()
+# Connect the CPU ports up to the membus
 system.cpu.icache_port = system.membus.cpu_side_ports
 system.cpu.dcache_port = system.membus.cpu_side_ports
 
+# create the interrupt controller for the CPU and connect to the membus
 system.cpu.createInterruptController()
 system.cpu.interrupts[0].pio = system.membus.mem_side_ports
 system.cpu.interrupts[0].int_requestor = system.membus.cpu_side_ports
 system.cpu.interrupts[0].int_responder = system.membus.mem_side_ports
 
 system.system_port = system.membus.cpu_side_ports
-
+# Create a DDR3 memory controller and connect it to the membus
 system.mem_ctrl = MemCtrl()
 system.mem_ctrl.dram = DDR3_1600_8x8()
 system.mem_ctrl.dram.range = system.mem_ranges[0]
